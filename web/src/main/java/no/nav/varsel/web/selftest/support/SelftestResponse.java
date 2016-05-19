13227d83d54b92f4eca12ba12e21bb5e99ae5522
Andreas Berg Skomedal <andreas.skomedal@nav.no>
2016-05-19 09:47:48 +0200

    java 8, fix contextroot appconfig

--- web/src/main/java/no/nav/varsel/web/selftest/support/SelftestResponse.java
+++ web/src/main/java/no/nav/varsel/web/selftest/support/SelftestResponse.java
@@ -3,8 +3,8 @@ package no.nav.varsel.web.selftest.support;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.Lists;
-import org.joda.time.LocalDateTime;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
