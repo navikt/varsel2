a3e833a30bb3612b35bbf4e2ebc4efec629c6073
Andreas Berg Skomedal <andreas.skomedal@nav.no>
2016-07-08 09:29:56 +0200

    NO_JIRA restmock 404

--- ws-consumer/src/test/java/no/nav/varsel/config/RestMock.java
+++ ws-consumer/src/test/java/no/nav/varsel/config/RestMock.java
@@ -1,13 +1,16 @@
 package no.nav.varsel.config;
 
 import static no.nav.varsel.wsconsumer.dokkat.support.VarselInfoMapperTest.createVarselInfo;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.mock.http.client.MockClientHttpRequest;
 import org.springframework.test.web.client.ResponseCreator;
@@ -39,7 +42,11 @@ public class RestMock {
 
 		restTemplate.setRequestFactory((uri, httpMethod) -> {
 			MockClientHttpRequest mockClientHttpRequest = new MockClientHttpRequest(httpMethod, uri);
-			mockClientHttpRequest.setResponse(mocks.get(uri.toString()).createResponse(mockClientHttpRequest));
+			if (mocks.containsKey(uri.toString())) {
+				mockClientHttpRequest.setResponse(mocks.get(uri.toString()).createResponse(mockClientHttpRequest));
+			} else {
+				mockClientHttpRequest.setResponse(withStatus(NOT_FOUND).createResponse(mockClientHttpRequest));
+			}
 			return mockClientHttpRequest;
 		});
 		return mocks;
