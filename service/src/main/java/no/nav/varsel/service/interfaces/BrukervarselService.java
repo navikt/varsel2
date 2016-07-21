d611b6d8c63a5c2dd0ba17e55e728d85fdca684a
Roar Bjurstrom <roar.bjurstrom@nav.no>
2016-07-21 15:58:56 +0200

    PKANAKONDA-1927 javadoc

--- service/src/main/java/no/nav/varsel/service/interfaces/BrukervarselService.java
+++ service/src/main/java/no/nav/varsel/service/interfaces/BrukervarselService.java
@@ -4,9 +4,17 @@ import no.nav.varsel.service.tvarsel005.to.HentVarselForBrukerResponseTo;
 import no.nav.varsel.service.tvarsel005.to.HentVarselForBrukerTo;
 
 /**
+ * Interface for Tvarsel005 - HentVarselForBruker
+ *
  * @author Roar Bjurstrom, Visma Consulting.
  */
 public interface BrukervarselService {
 
+	/**
+	 * Finds relevant varsels in a given period, based on fnr or aktorid
+	 *
+	 * @param reqTo The input object
+	 * @return the varsels
+	 */
 	HentVarselForBrukerResponseTo hentVarselForBruker(HentVarselForBrukerTo reqTo);
 }
