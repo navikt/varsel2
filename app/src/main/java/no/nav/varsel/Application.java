046aa92e3daaf1e2a07613e3d5fa635513bc0bfd
S161464 <hakon.snotun@nav.no>
2022-03-23 11:28:57 +0100

    MMA-6062: PR updates

--- app/src/main/java/no/nav/varsel/Application.java
+++ app/src/main/java/no/nav/varsel/Application.java
@@ -1,19 +1,15 @@
 package no.nav.varsel;
 
-import com.codahale.metrics.servlets.MetricsServlet;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.varsel.config.AppConfig;
 import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Servlet 3.0 Spring Boot Application Initializer for Varsel
@@ -27,24 +23,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		try {
-			SpringApplication.run(Application.class, args);
-		}catch (Exception e) {
-			log.error(e.getMessage());
-			throw e;
-		}
-	}
-
-	@Bean
-	public WebMvcConfigurerAdapter dispatcherServletConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addResourceHandlers(ResourceHandlerRegistry registry) {
-				// static content for selftest etc
-				registry.addResourceHandler("/internal/*")
-						.addResourceLocations("classpath:/web/static/css/");
-			}
-		};
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean(name = "webServiceServlet")
@@ -54,16 +33,6 @@ public class Application extends SpringBootServletInitializer {
 		servletRegistrationBean.setName("webServiceServlet");
 		servletRegistrationBean.addUrlMappings("/ws/*");
 		servletRegistrationBean.setLoadOnStartup(2);
-		return servletRegistrationBean;
-	}
-
-	@Bean
-	public ServletRegistrationBean metricsServlet() {
-		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
-		servletRegistrationBean.setServlet(new MetricsServlet());
-		servletRegistrationBean.setName("metricsServlet");
-		servletRegistrationBean.addUrlMappings("/internal/metrics/*");
-		servletRegistrationBean.setLoadOnStartup(3);
 		return servletRegistrationBean;
 	}
 
--- app/src/main/java/no/nav/varsel/config/AppConfig.java
+++ app/src/main/java/no/nav/varsel/config/AppConfig.java
@@ -1,9 +1,7 @@
 package no.nav.varsel.config;
 
-import com.codahale.metrics.servlets.MetricsServlet;
 import no.nav.varsel.config.alias.MqGatewayProperties;
 import no.nav.varsel.nais.NaisContract;
-import no.nav.varsel.web.metrics.MetricsServletContextListener;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -25,14 +23,8 @@ import org.springframework.context.annotation.Import;
 		ServiceConfig.class,
 		JmsConsumerConfig.class,
 		ProviderWsConfig.class,
-		SelftestConfig.class,
 		RetryLoggingInterceptor.class
 })
 public class AppConfig {
-
-	@Bean
-	public MetricsServlet.ContextListener metricContextListener() {
-		return new MetricsServletContextListener();
-	}
 
 }
