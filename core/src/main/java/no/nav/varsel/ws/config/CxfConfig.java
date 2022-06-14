ae6f7bed11ce149b3ee6862367bcccd725bed2c8
Christian Tumyr <christian.tumyr@nav.no>
2022-06-14 10:49:14 +0200

    Fjern WSAddressing feature
    Co-authored-by: Anders Opskar Voldsund <anders.voldsund@nav.no>

--- core/src/main/java/no/nav/varsel/ws/config/CxfConfig.java
+++ core/src/main/java/no/nav/varsel/ws/config/CxfConfig.java
@@ -2,11 +2,7 @@ package no.nav.varsel.ws.config;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBus;
-import org.apache.cxf.feature.Feature;
-import org.apache.cxf.ws.addressing.WSAddressingFeature;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Collection;
 
 /**
  * Spring Cxf Config
@@ -17,9 +13,6 @@ public class CxfConfig {
 
 	@Bean(name = Bus.DEFAULT_BUS_ID)
 	public SpringBus springBus() {
-		SpringBus springBus = new SpringBus();
-		Collection<Feature> features = springBus.getFeatures();
-		features.add(new WSAddressingFeature());
-		return springBus;
+		return new SpringBus();
 	}
 }
