ca44aa861f19232a6e9b81e7d8996b12af7bf3bb
Tsigab <tsigab.angosom@gmail.com>
2023-11-06 11:19:26 +0100

    MMA-6929 slettet duplikat bean HttpClientConnectionManager

--- core/src/main/java/no/nav/varsel/consumer/config/WsConsumerConfig.java
+++ core/src/main/java/no/nav/varsel/consumer/config/WsConsumerConfig.java
@@ -3,9 +3,6 @@ package no.nav.varsel.consumer.config;
 import no.nav.varsel.consumer.pdl.PdlIdentConsumer;
 import no.nav.varsel.consumer.sts.StsRestConsumer;
 import no.nav.varsel.ws.config.CxfConfig;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
-import org.apache.hc.client5.http.io.HttpClientConnectionManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -18,13 +15,5 @@ import org.springframework.context.annotation.Import;
 })
 @Configuration
 public class WsConsumerConfig {
-
-	@Bean
-	HttpClientConnectionManager httpClientConnectionManager() {
-		PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-		connectionManager.setMaxTotal(400);
-		connectionManager.setDefaultMaxPerRoute(100);
-		return connectionManager;
-	}
 
 }
