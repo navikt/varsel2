870b4186be2f46f525d4b78f8ae7d970435d0447
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2024-01-12 11:47:54 +0100

    MMA-7235 Oppdaterte avhengigheter og ryddet
    * Fikset deprekert kode og konfigurasjon

--- app/src/main/java/no/nav/varsel/config/RetryLoggingInterceptor.java
+++ app/src/main/java/no/nav/varsel/config/RetryLoggingInterceptor.java
@@ -1,22 +1,19 @@
 package no.nav.varsel.config;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.RetryCallback;
 import org.springframework.retry.RetryContext;
-import org.springframework.retry.listener.RetryListenerSupport;
+import org.springframework.retry.RetryListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
-public class RetryLoggingInterceptor extends RetryListenerSupport {
-	private static final Logger LOG = LoggerFactory.getLogger(RetryLoggingInterceptor.class);
+public class RetryLoggingInterceptor implements RetryListener {
 
 	@Override
 	public <T, E extends Throwable> void onError(RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
-		super.onError(context, callback, throwable);
 		// Logg retry uten stacktrace
-		LOG.info(String.format("Retry trigget for %s. gang med feilmelding=%s ", context.getRetryCount(), throwable.getMessage()));
+		log.info("Retry trigget for {}. gang med feilmelding={} ", context.getRetryCount(), throwable.getMessage());
 	}
-
 }
